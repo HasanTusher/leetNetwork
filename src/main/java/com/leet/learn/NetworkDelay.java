01a97031e7a08f4e0fdd99c6083aa6f0a676e19b
tusher787 <hasan.md@genweb2.com>
2021-08-23 15:18:07 +0600

    in single file

--- src/main/java/com/leet/learn/NetworkDelay.java
+++ src/main/java/com/leet/learn/NetworkDelay.java
@@ -1,24 +1,107 @@
 package com.leet.learn;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class NetworkDelay {
 
+    public class Result {
+        int cost;
+        int iteration;
+        List<Integer> traversed;
 
-    public static int networkDelayTime(int[][] times, int n, int k) {
+        public Result() {
+            this.traversed = new ArrayList<Integer>();
+        }
+
+        public List<Integer> getTraversed() {
+            return traversed;
+        }
+
+        public void setTraversed(List<Integer> traversed) {
+            this.traversed = traversed;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+
+        public void setCost(int cost) {
+            this.cost = cost;
+        }
+
+        public int getIteration() {
+            return iteration;
+        }
+
+        public void setIteration(int iteration) {
+            this.iteration = iteration;
+        }
+
+        @Override
+        public String toString() {
+            return "Result{" +
+                    "cost=" + cost +
+                    ", iteration=" + iteration +
+                    ", integerList=" + traversed +
+                    '}';
+        }
+    }
+
+
+    public class Dfs {
+
+
+        /**
+         *
+         *  {2,1,1}, // source, dest, time
+         *  {2,3,1},
+         *  {3,4,1}
+         *
+         * @param arr
+         * @param startingIndex
+         * @return
+         */
+        public Result getResultByDfs(int[][] arr, int startingIndex){
+            Stack<Integer> integerStack = new Stack<Integer>();
+            Result result = new Result();
+            result.setIteration(1);
+            result.setCost(arr[startingIndex][2]);
+
+            integerStack.push(arr[startingIndex][1]);
+            //int source = arr[startingIndex][0];
+            while (!integerStack.isEmpty()){
+                int popped = integerStack.pop();
+                result.getTraversed().add(popped);
+                for(int i = 0; i< arr.length; i++){
+                    if(arr[i][0] == popped){ //found
+                        //push in the stack
+                        integerStack.push(arr[i][1]);
+                        result.setIteration(result.getIteration()+1);
+                        result.setCost(result.getCost()+arr[i][2]);
+                    }
+                }
+
+            }
+            return result;
+        }
+
+
+    }
+
+
+    public  int networkDelayTime(int[][] times, int n, int k) {
+        Dfs dfs = new Dfs();
         Set<Integer> integerSet = new HashSet<Integer>();
         int cost = 0;
         integerSet.add(k);
         for(int i=0; i<times.length; i++){
             if(times[i][0] == k){
-                Result result = Dfs.getResultByDfs(times, i);
+                Result result = dfs.getResultByDfs(times, i);
                 if(result.getCost() > cost){
                     cost = result.getCost();
                 }
                 integerSet.addAll(result.getTraversed());
-                //System.out.println(result.toString());
+                System.out.println(result.toString());
             }
         }
 
@@ -28,6 +111,7 @@ public class NetworkDelay {
         return cost;
     }
     public static void main(String[] args) {
+        NetworkDelay networkDelay = new NetworkDelay();
         int[][] times = {
                 {2,1,1}, // source, dest, time
                 {2,3,1},
@@ -39,7 +123,7 @@ public class NetworkDelay {
         integerStack.push(1);
         int a = 1;
 
-        networkDelayTime(times, 4, 2);
+        networkDelay.networkDelayTime(times, 4, 2);
         System.out.println(integerStack.contains(a));
     }
 }
