f43db229287959f93d9a721c1aed04e0e2fc716c
tusher787 <hasan.md@genweb2.com>
2021-08-23 13:07:24 +0600

    result-iteration created;

---
+++ src/main/java/com/leet/learn/Result.java
@@ -0,0 +1,22 @@
+package com.leet.learn;
+
+public class Result {
+    int cost;
+    int iteration;
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getIteration() {
+        return iteration;
+    }
+
+    public void setIteration(int iteration) {
+        this.iteration = iteration;
+    }
+}
