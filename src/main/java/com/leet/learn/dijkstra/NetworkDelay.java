ad8be9240eb087fae8451ae03a5b79145c1dca81
tusher787 <hasan.md@genweb2.com>
2021-08-24 17:23:28 +0600

    finally done

--- src/main/java/com/leet/learn/dijkstra/NetworkDelay.java
+++ src/main/java/com/leet/learn/dijkstra/NetworkDelay.java
@@ -92,22 +92,29 @@ public class NetworkDelay {
     public  int networkDelayTime(int[][] times, int n, int k) {
         Dijkstra dijkstra = new Dijkstra();
         HashMap<Integer, Integer> integerIntegerHashMap = dijkstra.getShortestPathToAll(times, k);
-        System.out.println(integerIntegerHashMap);
-        return 0;
+        int size = integerIntegerHashMap.size();
+        if(size < n)
+            return -1;
+        int result = 0;
+        for(HashMap.Entry<Integer, Integer> item: integerIntegerHashMap.entrySet()){
+            if(result < item.getValue()) result = item.getValue();
+        }
+
+        return result;
     }
     public static void main(String[] args) {
         NetworkDelay networkDelay = new NetworkDelay();
         int[][] times = {
-                {2,1,1}, // source, dest, time
-                {2,3,1},
-                {3,2,1}
-//                {1,2,1}, // source, dest, time
-//                {2,3,2},
-//                {1,3,4}
+//                {2,1,1}, // source, dest, time
+//                {2,3,1},
+//                {3,2,1}
+                {1,2,1}, // source, dest, time
+                {2,3,2},
+                {1,3,2}
         };
 
-        networkDelay.networkDelayTime(times, 3, 1);
-        networkDelay.networkDelayTime(times, 4, 2);
+        //networkDelay.networkDelayTime(times, 3, 1);
+        System.out.println(networkDelay.networkDelayTime(times, 3, 1));
         //System.out.println(integerStack.contains(a));
     }
 }
